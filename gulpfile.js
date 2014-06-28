c8f280b447d5c447297bf93464c9e8ec9395d2eb
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2014-06-28 15:47:20 +0200

    Gulp no linting while developing

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ var paths = {
 };
 
 gulp.task('default', ['dev']);
-gulp.task('dev', ['lint', 'test', 'watch']);
+gulp.task('dev', ['test', 'watch']);
 
 gulp.task('watch', function () {
   gulp.watch([paths.src, paths.test], ['test']);
