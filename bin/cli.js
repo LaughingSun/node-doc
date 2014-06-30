7ecb9adb9055f63fa4c30a38aba2244d3f7493f6
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2014-06-30 21:29:18 +0200

    CLI 0.2

--- bin/cli.js
+++ bin/cli.js
@@ -7,38 +7,22 @@ var cli = require('commander')
 
 cli
   .version(pkg.version)
-  .usage('<dir> [options]')
-  .option('-o, --output <dir>', 'Location to output the results.')
-  .option('-r, --result <value>', 'Wether to output in Markdown or object.')
-  .option('-p, --private', 'Wether or not to show private comments.')
+  .usage('[options]')
+  .option('-o, --output <dir>', 'Directory to save the results to.')
+  .option('-r, --result <value>', 'Result type, available are Markdown, HTML or json (default).')
+  .option('-p, --private', 'Show private functions and comments.')
   .parse(process.argv);
 
-if (cli.args.length === 0) {
-  cli.help();
-}
+if (cli.args.length === 0) cli.help();
 
-var input = cli.args[0]
-  , output = cli.output || 'doc'
-  , options = {
-    result: cli.result || 'markdown',
-    private: cli.private || false
-  };
+var options = {
+  output = path.resolve(process.cwd(), cli.output || 'doc'),
+  result: cli.result || 'markdown',
+  private: cli.private || false
+};
 
-input = path.resolve(process.cwd(), input);
-output = path.resolve(process.cwd(), output);
+parser(options, function (err, doc) {
+  if (err) return console.error(err.message);
 
-var posCwd = process.cwd().length + 1
-  , convert = options.result.charAt(0).toUpperCase() + options.result.slice(1);
-
-console.log('Converting comments to ' + convert + '.');
-console.log('Input:  ' + input.slice(posCwd));
-console.log('Output: ' + output.slice(posCwd));
-
-try {
-  parser(input, output, options);
-} catch (err) {
-  console.error('\x1B[31m' + err.message + '\x1B[39m');
-  process.exit(1);
-}
-
-console.log('Done. See output dir for the results.');
+  console.log('See ' + options.output + ' for the documentation.');
+});
