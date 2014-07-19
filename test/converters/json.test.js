669bb87c31409debab0b42060764c59d1856b914
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2014-07-20 01:07:39 +0200

    Add json converter test

---
+++ test/converters/json.test.js
@@ -0,0 +1,36 @@
+require('should');
+
+describe('JSON converter', function () {
+  var prepareJson = require('../../lib/converters/json');
+
+  it('should add the main doc to main.json on the returning object', function () {
+    var doc = {
+          name: 'my-app'
+        }
+      , files = prepareJson(doc);
+
+    files.should.be.an.Object.and.have.property('main.json');
+    files['main.json'].should.equal(JSON.stringify(doc));
+  });
+
+  it('should add namespaces in it\'s own files', function () {
+    var doc = {
+          name: 'my-app',
+          ns: {
+            ns1: {
+              name: 'Namespace 1'
+            },
+            ns2: {
+              name: 'Namespace 2'
+            }
+          }
+        }
+      , files = prepareJson(doc);
+
+    files.should.be.an.Object.and.have.properties('main.json', 'ns1.json', 'ns2.json');
+    files['ns1.json'].should.equal(JSON.stringify(doc.ns.ns1));
+    files['ns2.json'].should.equal(JSON.stringify(doc.ns.ns2));
+    delete doc.ns;
+    files['main.json'].should.equal(JSON.stringify(doc));
+  });
+});
