0ff525b669b36b3feadc8de0259922bbca85832e
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2014-07-19 23:55:47 +0200

    Add save options

--- test/save.test.js
+++ test/save.test.js
@@ -1,5 +1,45 @@
-describe('Save', function () {
-  var save = require('../lib/save');
+var fs = require('fs')
+  , path = require('path');
 
-  it('should save the documentation object');
+describe('Save', function () {
+  var save = require('../lib/save')
+    , dir = path.resolve(__dirname, 'files');
+
+  it('should save the documentation object', function (cb) {
+    var file = path.resolve(dir, 'main.json')
+      , doc = JSON.stringify({
+          name: 'my-app'
+        });
+
+    save(dir, {
+      'main.json': doc
+    }, function () {
+      fs.readFileSync(file, 'utf8').should.equal(doc);
+      fs.unlinkSync(file)
+      cb();
+    });
+  });
+
+  it('should save the namespace files', function (cb) {
+    var file = path.resolve(dir, 'main.json')
+      , file2 = path.resolve(dir, 'ns1.json')
+      , doc = JSON.stringify({
+          name: 'my-app'
+        })
+      , doc2 = JSON.stringify({
+          name: 'Namespace 1'
+        });
+
+    save(dir, {
+      'main.json': doc,
+      'ns1.json': doc2
+    }, function () {
+      fs.readFileSync(file, 'utf8').should.equal(doc);
+      fs.unlinkSync(file)
+
+      fs.readFileSync(file2, 'utf8').should.equal(doc2);
+      fs.unlinkSync(file2)
+      cb();
+    });
+  });
 });
