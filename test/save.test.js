b7a0be77cbcac58ebebd778ccbae0369912e58e8
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2014-07-20 14:55:34 +0200

    Update save clean output dir and create it
    Create the output dir if it doesn’t exists and clean it if it does

--- test/save.test.js
+++ test/save.test.js
@@ -1,9 +1,21 @@
 var fs = require('fs')
-  , path = require('path');
+  , path = require('path')
+  , rimraf = require('rimraf');
 
 describe('Save', function () {
   var save = require('../lib/save')
-    , dir = path.resolve(__dirname, 'files');
+    , dir = path.resolve(__dirname, 'files/save')
+    , tmpPath = path.resolve(dir, 'tmp.json')
+    , tmpFile;
+
+
+  before(function () {
+    tmpFile = fs.readFileSync(tmpPath, 'utf8');
+  });
+
+  after(function () {
+    fs.writeFileSync(tmpPath, tmpFile, 'utf8');
+  });
 
   it('should save the documentation object', function (cb) {
     var file = path.resolve(dir, 'main.json')
@@ -15,7 +27,7 @@ describe('Save', function () {
       'main.json': doc
     }, function () {
       fs.readFileSync(file, 'utf8').should.equal(doc);
-      fs.unlinkSync(file)
+      fs.unlinkSync(file);
       cb();
     });
   });
@@ -35,10 +47,45 @@ describe('Save', function () {
       'ns1.json': doc2
     }, function () {
       fs.readFileSync(file, 'utf8').should.equal(doc);
-      fs.unlinkSync(file)
-
       fs.readFileSync(file2, 'utf8').should.equal(doc2);
-      fs.unlinkSync(file2)
+      fs.unlinkSync(file);
+      fs.unlinkSync(file2);
+      cb();
+    });
+  });
+
+  it('should clean the output dir', function (cb) {
+    var file = path.resolve(dir, 'main.json')
+      , doc = JSON.stringify({
+          name: 'my-app'
+        });
+
+    save(dir, {
+      'main.json': doc
+    }, function () {
+      fs.readFileSync(file, 'utf8').should.equal(doc);
+
+      (function () {
+        var stat = fs.statSync(tmpPath);
+      }).should.throw('ENOENT, no such file or directory \'' + tmpPath + '\'');
+
+      fs.unlinkSync(file);
+      cb();
+    });
+  });
+
+  it('should create the output dir if non existing', function (cb) {
+    var dir2 = path.resolve(dir, 'not-here')
+      , file = path.resolve(dir2, 'main.json')
+      , doc = JSON.stringify({
+          name: 'my-app'
+        });
+
+    save(dir2, {
+      'main.json': doc
+    }, function () {
+      fs.readFileSync(file, 'utf8').should.equal(doc);
+      rimraf.sync(dir2);
       cb();
     });
   });
