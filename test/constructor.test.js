7bdd4be8504dff7748a4c04388786282c4f854cf
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2014-08-14 02:56:16 +0200

    Replace own getpackage with sak-getpackage

--- test/constructor.test.js
+++ test/constructor.test.js
@@ -12,7 +12,7 @@ describe('Constructor', function () {
     it('should add extra information to the documentation object', function () {
       var doc = constructDoc({
         name: 'My app',
-        author: 'Thomas de Zeeuw',
+        author: {name: 'Thomas de Zeeuw'},
         description: 'My awesome application',
         license: 'MIT',
         version: '0.0.0'
@@ -29,7 +29,7 @@ describe('Constructor', function () {
     it('should not add extra information to the documentation object if it\'s an namespace object', function () {
       var doc = constructDoc({
         name: 'My app',
-        author: 'Thomas de Zeeuw',
+        author: {name: 'Thomas de Zeeuw'},
         description: 'My awesome application',
         license: 'MIT',
         version: '0.0.0',
@@ -42,62 +42,6 @@ describe('Constructor', function () {
       should.equal(doc.desc, undefined);
       should.equal(doc.license, undefined);
       should.equal(doc.version, undefined);
-    });
-  });
-
-  describe('Parse Author', function () {
-    it('should parse author correctly with both email and website', function () {
-      var doc = constructDoc({
-        name: 'My app',
-        author: 'Thomas de Zeeuw <thomasdezeeuw@gmail.com> (https://thomasdezeeuw.nl/)'
-      }, [], {});
-
-      doc.should.be.an.Object;
-      doc.name.should.equal('My app');
-      doc.author.should.be.an.Object;
-      doc.author.name.should.equal('Thomas de Zeeuw');
-      doc.author.email.should.equal('thomasdezeeuw@gmail.com');
-      doc.author.website.should.equal('https://thomasdezeeuw.nl/');
-    });
-
-    it('should parse author correctly with both email and website', function () {
-      var doc = constructDoc({
-        name: 'My app',
-        author: 'Thomas de Zeeuw (https://thomasdezeeuw.nl/) <thomasdezeeuw@gmail.com>'
-      }, [], {});
-
-      doc.should.be.an.Object;
-      doc.name.should.equal('My app');
-      doc.author.should.be.an.Object;
-      doc.author.name.should.equal('Thomas de Zeeuw');
-      doc.author.email.should.equal('thomasdezeeuw@gmail.com');
-      doc.author.website.should.equal('https://thomasdezeeuw.nl/');
-    });
-
-    it('should parse author correctly with email', function () {
-      var doc = constructDoc({
-        name: 'My app',
-        author: 'Thomas de Zeeuw <thomasdezeeuw@gmail.com>'
-      }, [], {});
-
-      doc.should.be.an.Object;
-      doc.name.should.equal('My app');
-      doc.author.should.be.an.Object;
-      doc.author.name.should.equal('Thomas de Zeeuw');
-      doc.author.email.should.equal('thomasdezeeuw@gmail.com')
-    });
-
-    it('should parse author correctly with website', function () {
-      var doc = constructDoc({
-        name: 'My app',
-        author: 'Thomas de Zeeuw (https://thomasdezeeuw.nl/)'
-      }, [], {});
-
-      doc.should.be.an.Object;
-      doc.name.should.equal('My app');
-      doc.author.should.be.an.Object;
-      doc.author.name.should.equal('Thomas de Zeeuw');
-      doc.author.website.should.equal('https://thomasdezeeuw.nl/');
     });
   });
 });
